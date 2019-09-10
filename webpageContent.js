c64eeef70840fb48b7ac6d94578ea348cfca530c
Mona Lisa <patrinoua@gmail.com>
2019-09-10 03:50:57 +0200

    first commit

---
+++ webpageContent.js
@@ -0,0 +1,40 @@
+exports.contentEn = {
+  name: 'Kostis Lazaridis',
+
+  about: 'About',
+  work: 'Work',
+  contact: 'Contact',
+  detailedCv: 'Detailed CV',
+  shortCv: 'My CV',
+  title: 'Kostis Lazaridis',
+
+  personalDescription: `
+        Kostis is very good in fluid and solid material science but he usually loses in Backgammon (tavli).
+        He would like to develop and hopes that one day he will be as good as Angeliki.
+        Until then, he is studying about nature, the universe and the interfaces between different states of materials.
+        If you have any questions about these maters do not hesitate to contact him!
+            `,
+
+  myFont: 'Quicksand'
+}
+exports.contentGr = {
+  name: 'Κωστής Λαζαρίδης',
+
+  about: 'Σχετικά',
+  work: 'Πορτφόλιο',
+  contact: 'Επικοινωνία',
+  detailedCv: 'Αναλυτικό Βιογραφικό',
+  shortCv: 'Βιογραφικό',
+  title: 'Κωστής Λαζαρίδης',
+
+  personalDescription: `
+        Με λένε Αγγελική, είμαι δημιουργός ιστοσελίδων
+        και τα τελευταία χρόνια ζω και εργάζομαι στο Βερολίνο.
+        Έχω σπουδάσει Εφαρμοσμένα Μαθηματικα και Διοίκηση Επιχειρήσεων
+        και στο παρελθόν έχω εργαστεί στο Online Marketing,
+        ως Σύμβουλος Επιχειρήσεων αλλά και ως Software Engineer.
+        Μου αρέσει να φτιάχνω εύχρηστες ιστοσελίδες με ωραίο design,
+        χρησιμοποιώντας της τελευταίες τεχνολογίες.
+      `,
+  myFont: 'Helvetica'
+}
