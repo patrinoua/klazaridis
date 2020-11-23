3939dcdbb349638990244540dea0c19fbda288eb
Mona Lisa <patrinoua@gmail.com>
2020-11-23 10:57:24 +0100

    change text

--- webpageContent.js
+++ webpageContent.js
@@ -1,22 +1,21 @@
 exports.contentEn = {
-  name: 'Kostis Lazaridis',
+  name: "Kostis Lazaridis",
 
-  about: 'About',
-  work: 'Work',
-  contact: 'Contact',
-  detailedCv: 'Detailed CV',
-  shortCv: 'My CV',
-  title: 'Kostis Lazaridis',
+  about: "About",
+  work: "Work",
+  contact: "Contact",
+  detailedCv: "Detailed CV",
+  shortCv: "My CV",
+  title: "Kostis Lazaridis",
 
   personalDescription: `
-  A highly motivated Ph.D. candidate in Mechanical Engineering specialized in Fluid Dynamics with a
-  particular interest on interface phenomena for coupled moving boundaries problems
-  (wetting). My primary goal is to utilize my mathematical background in Partial Differential Equations to solve problems in Mathematical Physics.
-  Specifically, current interests include:
-  Mathematical formulation of physical processes, with particular interest in multiscale
+  Ph.D. candidate in Mechanical Engineering specialized in Fluid Dynamics with a
+  particular interest on interface phenomena for coupled moving boundaries problems (wetting). 
+  Kostis utilizes his mathematical background in Partial Differential Equations to solve problems in Mathematical Physics.
+  His current interests include Mathematical formulation of physical processes, with particular interest in multiscale
   models and computational methods, and with applications in both, materials processing
   and service.
               `,
 
-  myFont: 'Quicksand'
+  myFont: "Quicksand"
 }
